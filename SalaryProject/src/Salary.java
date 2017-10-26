685cb373897da85f9dcc9f55ab1d8328958ea3b8
Jacovey <jaspercovey@gmail.com>
2017-10-26 08:49:08 -0400

    up2

--- SalaryProject/src/Salary.java
+++ SalaryProject/src/Salary.java
@@ -1,4 +1,16 @@
 
 public class Salary {
-
+	private double annual;
+	public Salary(double salary) {
+		if (salary>=0) annual=salary;
+	}
+	public void setAnnualSalary(double salary) {
+		if (salary>=0) annual=salary;
+	}
+	public double getAnnualSalary() {
+		return annual;
+	}
+	public String toString() {
+		return "$"+annual;
+	}
 }
---
+++ SalaryProject/src/TestSalaries.java
@@ -0,0 +1,10 @@
+
+public class TestSalaries {
+	public static void main (String[] args) {
+		Salary s1 = new Salary(64100);
+		System.out.println(s1);
+		
+		Salary s2 = new Salary(400.0);
+		System.out.println(s2);
+	}
+}
