33dd704ccccd8c1a5734a52eccf45f6941d08300
Jacovey <jaspercovey@gmail.com>
2017-10-27 10:57:34 -0400

    Updated Salary Class to make error messages

--- SalaryProject/src/Salary.java
+++ SalaryProject/src/Salary.java
@@ -3,9 +3,11 @@ public class Salary {
 	private double annual;
 	public Salary(double salary) {
 		if (salary>=0) annual=salary;
+		else System.out.println("Cannot set salary to that");
 	}
 	public void setAnnualSalary(double salary) {
 		if (salary>=0) annual=salary;
+		else {System.out.println("Cannot set salary to that"); annual=0;}
 	}
 	public double getAnnualSalary() {
 		return annual;
