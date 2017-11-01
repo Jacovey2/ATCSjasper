371a101ae796edd5e6553bad90c736b7d7f910e8
Jacovey <jaspercovey@gmail.com>
2017-11-01 10:35:17 -0400

    added required methods and updated multi-value methods

--- BetterArray/src/BetterIntArray.java
+++ BetterArray/src/BetterIntArray.java
@@ -2,6 +2,10 @@
 public class BetterIntArray {
 	private int[] arr;
 
+	public BetterIntArray() {
+		arr = new int[0];
+	}
+
 	public BetterIntArray(int[] setupArray) {
 		arr = setupArray;
 	}
@@ -14,7 +18,7 @@ public class BetterIntArray {
 		return numfound;
 	}
 
-	public void add(int val) {
+	public void add(int val) {// push
 		int[] tempArr = new int[arr.length + 1];
 		for (int i = 0; i < arr.length; i++)
 			tempArr[i] = arr[i];
@@ -22,33 +26,85 @@ public class BetterIntArray {
 		arr = tempArr;
 	}
 
-	public void add(int[] vals) {
-		for (int i = 0; i < vals.length; i++)
-			add(vals[i]);
-	}
-
-	public void pop() {
-		int[] tempArr = new int[arr.length - 1];
-		for (int i = 0; i < arr.length - 1; i++)
-			tempArr[i] = arr[i];
-		arr = tempArr;
-	}
-
-	public void remove(int val) {
-		int[] tempArr = new int[arr.length - contains(val)];
-		int index = 0;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] != val) {
-				tempArr[index] = arr[i];
-				index++;
+	public void add(int val, int position) {// insert
+		int[] tempArr = new int[arr.length + 1];
+		for (int i = 0; i < arr.length + 1; i++) {
+			if (i < position) {
+				tempArr[i] = arr[i];
+			} else if (i == position) {
+				tempArr[i] = val;
+			} else if (i > position) {
+				tempArr[i] = arr[i - 1];
 			}
 		}
 		arr = tempArr;
 	}
 
-	public void remove(int[] vals) {
+	public void add(int[] vals) {// push multiple
 		for (int i = 0; i < vals.length; i++)
-			remove(vals[i]);
+			add(vals[i]);
+	}
+
+	public void add(int[] vals, int startpos) {// insert multiple
+		for (int i = 0; i < vals.length; i++)
+			add(vals[i], startpos + i);
+	}
+
+	public int pop() {//pop last
+		int[] tempArr = new int[arr.length - 1];
+		int retval = arr[arr.length - 1];
+		for (int i = 0; i < arr.length - 1; i++)
+			tempArr[i] = arr[i];
+		arr = tempArr;
+		return retval;
+	}
+	public int pop(int position) {//indexed pop
+		int[] tempArr = new int[arr.length - 1];
+		int retval = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (i<position) 
+				tempArr[i] = arr[i];
+			else if (i==position)
+				retval=arr[i];
+			else if (i>position)
+				tempArr[i-1] = arr[i];
+		}
+		arr = tempArr;
+		return retval;
+	}
+	
+	public void swap(int pos1, int pos2) {
+		int l = arr.length;
+		if (pos1 < l && pos1 >= 0 && pos2 < l && pos2 >= 0) {
+			int store1 = arr[pos1];
+			int store2 = arr[pos2];
+			arr[pos2] = store1;
+			arr[pos1] = store2;
+		}
+	}
+
+	public boolean remove(int val) {
+		if (contains(val) > 0) {
+			int[] tempArr = new int[arr.length - contains(val)];
+			int index = 0;
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] != val) {
+					tempArr[index] = arr[i];
+					index++;
+				}
+			}
+			arr = tempArr;
+			return true;
+		} else
+			return false;
+	}
+
+	public boolean[] remove(int[] vals) {
+		boolean[] flags = new boolean[vals.length];
+		for (int i = 0; i < vals.length; i++) {
+			flags[i] = remove(vals[i]);
+		}
+		return flags;
 	}
 
 	public int size() {
@@ -79,16 +135,16 @@ public class BetterIntArray {
 		}
 		return s;
 	}
-	
+
 	public int[] toArray() {
 		return arr;
 	}
 
 	public void removeDuplicates() {
-		BetterIntArray tempBetIntArr= new BetterIntArray(new int[0]);
-		for (int i = 0; i < arr.length; i++) 
-			if (tempBetIntArr.contains(arr[i])==0)
+		BetterIntArray tempBetIntArr = new BetterIntArray(new int[0]);
+		for (int i = 0; i < arr.length; i++)
+			if (tempBetIntArr.contains(arr[i]) == 0)
 				tempBetIntArr.add(arr[i]);
-		arr=tempBetIntArr.toArray();
+		arr = tempBetIntArr.toArray();
 	}
 }
--- BetterArray/src/TestBetterIntArray.java
+++ BetterArray/src/TestBetterIntArray.java
@@ -4,30 +4,43 @@ public class TestBetterIntArray {
 		int[] nums = { 121, 142, 121, 154, 205 };
 		BetterIntArray numberArray = new BetterIntArray(nums);
 
-		System.out.println("Original                   :   " + numberArray.toString());
-		System.out.println("Size                       :   " + numberArray.size());
-		System.out.println("ValueAt(2)                 :   " + numberArray.valueAt(2));
-		System.out.println("IndexOf(142)               :   " + numberArray.indexOf(142)[0]);
-		System.out.println("Contains(121)              :   " + numberArray.contains(121));
+		System.out.println("Original                         ::   " + numberArray);
+		System.out.println("Size                             ::   " + numberArray.size());
+		System.out.println("ValueAt(2)                       ::   " + numberArray.valueAt(2));
+		System.out.println("IndexOf(142)                     ::   " + numberArray.indexOf(142)[0]);
+		System.out.println("Contains(121)                    ::   " + numberArray.contains(121));
 
-		numberArray.pop();
-		System.out.println("Pop                        :   " + numberArray.toString());
+		int retval= numberArray.pop();
+		System.out.println("Pop                              ::   " + numberArray +" :: "+retval);
+		retval= numberArray.pop(1);
+		System.out.println("Pop out 1                        ::   " + numberArray +" :: "+retval);
 
 		numberArray.add(200);
-		System.out.println("Added [200]                :   " + numberArray.toString());
+		System.out.println("Added [200]                      ::   " + numberArray);
+		numberArray.add(111,2);
+		System.out.println("Added [200] at 2                 ::   " + numberArray);
 
 		int[] tempArr1 = { 132, 234, 512, 234 };
 		numberArray.add(tempArr1);
-		System.out.println("Added [131, 234, 512, 234] :   " + numberArray.toString());
+		System.out.println("Added [131, 234, 512, 234]       ::   " + numberArray);
+		
+		int[] tempArr2 = { 545, 444 };
+		numberArray.add(tempArr2, 2);
+		System.out.println("Added [131, 234, 512, 234]  at 2 ::   " + numberArray);
 
 		numberArray.remove(121);
-		System.out.println("Removed [121]              :   " + numberArray.toString());
+		System.out.println("Removed [121]                    ::   " + numberArray);
 
-		int[] tempArr2 = { 142, 154 };
-		numberArray.remove(tempArr2);
-		System.out.println("Removed [142, 154]         :   " + numberArray.toString());
+		int[] tempArr3 = { 142, 154 };
+		numberArray.remove(tempArr3);
+		System.out.println("Removed [142, 154]               ::   " + numberArray);
 		
 		numberArray.removeDuplicates();
-		System.out.println("Removed Duplicates         :   " + numberArray.toString());
+		System.out.println("Removed Duplicates               ::   " + numberArray);
+		
+		numberArray.swap(0, 6);
+		System.out.println("Swap 0 and 5                     ::   " + numberArray);
+		
+		
 	}
 }
